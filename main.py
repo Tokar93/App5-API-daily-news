01ada0b551ce62c9a08c42bf630af9489938b6a5
tokarski-to <Tomasz.Tokarski@esk-sic.de>
2024-06-24 14:01:32 +0200

    Day 27: Create title with description string and send news to the email

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = '1551997252ec4f1394ee4da6fdb44c59'
 
@@ -11,7 +12,10 @@ request = requests.get(url)
 
 # Get a dictionary with data
 content = request.json()
-
+body = ''
 # Access the article titles and description
 for article in content['articles']:
-       print(article['title'])
+    if article['title'] is not None and article['description'] is not None:
+        body = body + article['title'].upper() + '\n' + article['description'] + 2*'\n'
+body.encode('utf-8')
+send_email(message=body)
