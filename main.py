8dff39f4967fd1c2c528ca863d639b07230cbf2e
Tomasz <tomasz.tokarski93@gmail.com>
2024-06-25 23:11:24 +0200

    Fix minor bugs

--- main.py
+++ main.py
@@ -2,27 +2,26 @@ import requests, os
 from send_email import send_email
 
 topic = 'tesla'
-api_key = os.getenv((API_KEY))
-url = ('https://newsapi.org/v2/everything?'
-       f'q={topic}&'
-       'from=2024-05-24&'
-       'sortBy=publishedAt&'
-       'apiKey=1551997252ec4f1394ee4da6fdb44c59&'
-       'language=en')
+#api_key = os.getenv((API_KEY))
+url = ("https://newsapi.org/v2/everything?"
+       "q=tesla&"
+       "from=2024-05-25&"
+       "sortBy=publishedAt&"
+       "apiKey=1551997252ec4f1394ee4da6fdb44c59&"
+       "language=en")
 
 # Make request
 request = requests.get(url)
 
 # Get a dictionary with data
 content = request.json()
-body = ''
+body = ""
 # Access the article titles and description
 for article in content['articles'][:20]:
-    if article['title'] is not None and article['description'] is not None:
-        body = ("Subject: Today's news"
-                + '\n' + body + article['title'].upper() + '\n'
+    if article['title'] is not None and article['description'] is not None and 'Removed' not in article['title']:
+        body = (body + article['title'].upper() + '\n'
                 + article['description']
                 + '\n' + article['url'] + 2*'\n')
-body.encode('utf-8')
-print(body)
-#send_email(message=body)
+body = 'Subject: News for today!\n' + body
+new_body = body.encode("utf-8")
+send_email(message=new_body)
